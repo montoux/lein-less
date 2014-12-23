adec6095f8975fd8c181541c5b489a8ee4cd6158
Stephen Nelson <stephen@montoux.com>
2014-12-24 11:50:56 +1300

    Version 1.7.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-less "1.7.3-SNAPSHOT"
+(defproject lein-less "1.7.3"
   :description "Less CSS compiler plugin for leiningen"
   :url "http://github.com/montoux/lein-less"
   :license {:name "Eclipse Public License"
